a7f8128767610286ad531199233b43f93592b1f2
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-04 04:25:46 +0100

    adds route to delete market

--- src/routes/market.ts
+++ src/routes/market.ts
@@ -10,5 +10,6 @@ router.post('/', auth, market.create);
 router.get('/', auth, market.getAll);
 router.get('/geo', auth, market.getByReverseGeocoding);
 router.get('/:category', auth, market.getByCategory);
+router.delete('/:name', auth, market.deleteMarket);
 
 export default router;
