4ad0d2ee2bfa5fa1af81dd73a394f21ea2cbfdb1
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-06 01:15:13 +0100

    adds more validations

--- src/validation/market.ts
+++ src/validation/market.ts
@@ -1,10 +1,34 @@
 import joi from '@hapi/joi';
 
 export const marketSchema = joi.object().keys({
-  name: joi.string().min(2).max(20).required(),
+  name: joi.string().min(2).max(255).required(),
   description: joi.string().min(6).required(),
   foodCategory: joi.string().min(2).required(),
   address: joi.string().min(10).required(),
   images: joi.array().items(joi.string()).required(),
-  cordeinates: joi.object(),
+  cordinates: joi.object(),
+});
+
+export const latlngSchema = joi.object().keys({
+  latlng: joi.string().required(),
+});
+export const categorySchema = joi.object().keys({
+  category: joi.string().required(),
+});
+
+export const updateSchema = joi.object().keys({
+  id: joi.string().required(),
+  name: joi.string(),
+  foodCategory: joi.string(),
+  description: joi.string(),
+  address: joi.string(),
+  images: joi.array().items(joi.string()),
+  cordinates: joi.object(),
+});
+export const updateNameSchema = joi.object().keys({
+  oldName: joi.string().required(),
+  newName: joi.string().required(),
+});
+export const deleteMarketsSchema = joi.object().keys({
+  markets: joi.array().items(joi.string()).required(),
 });
