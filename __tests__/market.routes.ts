9890d90602455d271217c1aee082427fd6a235d7
EstherSeyi <sheyiogundijo@gmail.com>
2020-05-07 19:03:50 +0100

    adds some more route tests

--- __tests__/market.routes.ts
+++ __tests__/market.routes.ts
@@ -2,7 +2,13 @@ import request from 'supertest';
 import app from '../src/app';
 
 import seed from '../testSetup/db/seed';
+import market from '../src/controller/market';
 const { connectMongoDB, disconnectMongoDB } = require('../testSetup/mongodb');
+import testHelp from '../src/utils/testHelp';
+
+const { mockRequest, mockResponse } = testHelp;
+
+let id: string;
 
 beforeAll(async () => {
   await connectMongoDB('marketTest');
@@ -25,9 +31,8 @@ describe('Markets', () => {
         name: 'Mile-12 Market',
         description:
           'Popularly known as the largest market for fresh foods and vegetables in West Africa, Mile12 International Market is estimated to have over 150,000 regular traders daily trading on different commodities, including traders from neighboring countries like Togo, Benin, Ghana, Cameroon, Niger and Chad',
-        foodCategory: 'grocery',
-        address:
-          '53 orisigun street kosofe mile 12 Lagos, Kosofe 300001, Lagos',
+        foodCategory: 'meat',
+        latlng: '9.0226177,7.4888433',
         images: [
           'https://bit.ly/2VTN8jc',
           'https://bit.ly/2VTN8jc',
@@ -35,6 +40,8 @@ describe('Markets', () => {
         ],
       })
       .expect((res) => {
+        id = res.body.payload._id;
+
         expect(res.status).toBe(200);
         expect(res.body.message).toBe('success');
         expect(res.body.payload).toEqual(
@@ -46,5 +53,44 @@ describe('Markets', () => {
           }),
         );
       });
+  });
+
+  test('gets single market', async () => {
+    return await request(app)
+      .get('/api/v1/market/single')
+      .query({ id: id })
+      .expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('success');
+      });
+  });
+
+  test('gets all market', async () => {
+    return await request(app)
+      .get('/api/v1/market')
+      .expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('success');
+        expect(res.body.payload).toEqual(expect.any(Array));
+      });
+  });
+  test('Find single market by category', async () => {
+    return await request(app)
+      .get('/api/v1/market/category')
+      .query({ searchBy: 'foodCategory', searchValue: 'fruits' })
+      .expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('success');
+        expect(res.body.payload).toEqual(expect.any(Array));
+      });
+  });
+  test('Gets nearest market', async () => {
+    let req = mockRequest();
+    req.params.market = '19 Daniel Makinde Street, Ikosi, Ketu, Lagos';
+    const res = mockResponse();
+    await market.getNearestMarket(req, res);
+
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send.mock.calls.length).toBe(1);
   });
 });
